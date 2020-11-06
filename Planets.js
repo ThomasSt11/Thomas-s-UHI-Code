aeacf2d0acba804ea4463ef78391d0952981f076
ThomasSt11 <74022819+ThomasSt11@users.noreply.github.com>
2020-11-06 09:07:18 +0000

    Add files via upload

---
+++ Planets.js
@@ -0,0 +1,15 @@
+var planets= new Array;
+var distance= new Array;
+
+var planets = ["Mercury ","Venus ","Earth ","Mars ","Jupiter ","Saturn ","Uranus ","Neptune ","Pluto "];
+var distance =["57.9", "108.2","149.6","227.9","778.3","1427","2871","4497.1","5913"];
+
+for (i = 0;   i <=8;   i++)
+    {
+        document.write( planets[i] + distance[i] + " Million Km away from the sun. "+ "<br>");
+    }
+
+
+
+
+
---
+++ nameClassVer2.js
@@ -0,0 +1,10 @@
+var Name= Array();
+var Name= ["Aidan","Emanuel","Cody","Daniel ","Jerry","Haiden ","Jasiu ","Kyle ","Liam ","Ethan ","Vaida ","Micheal ","Andrew ","Jean ","Maks ","Leah ","Rowan","Paige","Stephen ","Harry ","Kaidyn "];
+
+Name.sort();
+for (i=0;   i<Name.length;   i++)
+    {
+        document.write( i +") " + Name[i] + "<br>");
+    }
+    
+
